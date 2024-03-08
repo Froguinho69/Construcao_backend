254d3cac02573c48333eda6ad443f125ffd9f745
Froguinho69 <lrodrigues@iesb.edu.br>
2024-03-07 21:50:34 -0300

    aula dia 7

--- ListaDeExercicios/exercicio1.js
+++ ListaDeExercicios/exercicio1.js
@@ -1,4 +1,22 @@
 var prompt = require('prompt-sync')()
 let nome = prompt("Qual o seu nome? ")
 
-console.log("Olá " + nome + " Salve")
+console.log("Olá, " + nome + ". Salve")
+
+let idade = prompt("Tem quantus anus? ")
+
+if(idade >= 18){
+    console.log("Você já pode ser preso, pivete!")
+} else {
+    console.log("Ta muleke ainda!")
+}
+
+let notaProva1 = Number(prompt("Nota prova a1? "))
+let notaProva2 = Number(prompt("Nota prova a2? "))
+
+let media = (notaProva1 + notaProva2) / 2
+    if(media >= 6){
+        console.log("Passou mizerave")
+    } else {
+        console.log("vacilão morre cedo!")
+    }
---
+++ ListaDeExercicios/tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+let idade = prompt("Fala o nome ai.")
+
+if(idade >= 18){
+    console.log("Você já pode ser preso!")
+} else {
+    console.log("Ta muleke ainda!")
+}
